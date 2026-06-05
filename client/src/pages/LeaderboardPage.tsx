13797a5b246e82238f657f1feb9ccb19b95695bd
tigritik <tigritik@gmail.com>
2026-06-05 01:15:09 -0400

    removed hard coded leaderboard entries

--- client/src/pages/LeaderboardPage.tsx
+++ client/src/pages/LeaderboardPage.tsx
@@ -7,30 +7,7 @@ export default function LeaderboardPage() {
     const [data, setData] = useState<LeaderboardEntry[]>([]);
 
     useEffect(() => {
-        fetchLeaderboard()
-            .then(d => setData([...d,
-            {
-                id: "106c7899-879a-49e2-89e1-0c0b9dd8a98b",
-                name: "test",
-                rank: 1,
-                points: 11,
-                maxPoints: 100,
-            },
-            {
-                id: "106c7899-879a-49e2-89e1-0c0b9dd8a98b",
-                name: "test",
-                rank: 2,
-                points: 7,
-                maxPoints: 100,
-            },
-            {
-            id: "106c7899-879a-49e2-89e1-0c0b9dd8a98b",
-                name: "test",
-                rank: 3,
-                points: 5,
-                maxPoints: 100,
-            }
-        ]));
+        fetchLeaderboard().then(setData);
     }, []);
 
     return (
